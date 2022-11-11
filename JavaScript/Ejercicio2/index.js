9f261f06ec9566d25a2a67948ba5711537c56218
brab0210 <bernardoarrechea@gmail.com>
2022-11-11 20:31:05 -0300

    second commit

---
+++ JavaScript/Ejercicio2/index.js
@@ -0,0 +1,14 @@
+let miLista = [{
+    nombre: "Bernardo",
+    edad: 33,
+    dev: true,
+    date: new Date(1989, 1, 2),
+    book: {
+        titulo: "la piedra filosofal",
+        autor: "J.K. Rowling",
+        date: new Date(1997, 5, 26),
+        url: "https://es.wikipedia.org/wiki/Harry_Potter_y_la_piedra_filosofal"
+    },
+}];
+
+console.log(miLista);
