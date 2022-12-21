21922bfced77c3ddc73e89ee64146d5d7e9712da
brab0210 <bernardoarrechea@gmail.com>
2022-12-21 12:30:58 -0300

    excercise 5 done

---
+++ JavaScript/Ejercicio5/index.js
@@ -0,0 +1,12 @@
+let altura = 189;
+let alturaMts = 1.89;
+let peso = 98.5;
+let alturaRedondeada = Math.round(alturaMts)
+let pesoRedondeado = Math.floor(peso)
+let max_value = Number.MAX_VALUE;
+
+if(max_value === (max_value + 1)){
+    console.log("es igual");
+}
+
+
