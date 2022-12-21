a91e8356f22d46eb437ac4a8dfe243d0ff771583
brab0210 <bernardoarrechea@gmail.com>
2022-12-21 11:55:59 -0300

    excercise 4 done

---
+++ JavaScript/Ejercicio4/index.js
@@ -0,0 +1,12 @@
+let nombre = "Bernardo"
+let apellido = "Arrechea"
+let estudiante = `${nombre} ${apellido}`;
+let estudianteMayus = estudiante.toUpperCase()
+let estudianteMinus = estudiante.toLowerCase()
+let nroLetras = estudiante.length;
+let primeraLetra = nombre.charAt(0);
+let ultimaLetra = apellido.charAt(apellido.length - 1);
+let trimEstudiante = estudiante.trim();
+let matchEstudiante = estudiante.includes(nombre);
+
+
