374b9b811c3d49a6f5c47c3ebf34cb85e1451c2d
brab0210 <bernardoarrechea@gmail.com>
2022-12-28 16:29:46 -0300

    exercise 6 done

---
+++ JavaScript/Ejercicio6/index.js
@@ -0,0 +1,35 @@
+/*- Una nueva lista que concatene la lista de directores y la lista de los títulos (utilizando concat)
+
+- Una nueva lista que concatene la lista de directores y la lista de los títulos (utilizando el factor de propagación) */
+
+let compras = ["banana", "leche", "mandioca", "yogurt", "pescado", "carne"];
+compras.push("Aceite de Girasol");
+compras.pop();
+let peliculas = [
+  {
+    titulo: "Harry Potter",
+    director: "Scott Yard",
+    fecha: new Date("12/20/2001"),
+  },
+  {
+    titulo: "Avatar",
+    director: "James Cameron",
+    fecha: new Date("06/15/2009"),
+  },
+  {
+    titulo: "Avengers",
+    director: "Kevin Faged",
+    fecha: new Date("07/15/2013"),
+  },
+];
+
+let fechaEnero = new Date("01/01/2010");
+let peliculaFecha = peliculas.filter((element) => element.fecha > fechaEnero);
+
+let directores = peliculas.map((e) => e.director);
+let titulos = peliculas.map((e) => e.titulo);
+
+let newPeliculas = directores.concat(titulos);
+let peliculasSpread = [...directores, ...titulos];
+
+console.log(peliculasSpread);
